e8a34e5df1619f04201eb54c2e16784ab4338711
nidunc <nidunc@users.noreply.github.com>
2024-10-07 13:16:41 +0200

    Added missing return types

--- alien.py
+++ alien.py
@@ -27,12 +27,12 @@ class Alien(Sprite):
         # Store the alien’s exact horizontal position.
         self.x = float(self.rect.x)
 
-    def check_edges(self):
+    def check_edges(self) -> bool:
         """Return True if alien is at edge of screen."""
         screen_rect = self.screen.get_rect()
         return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
 
-    def update(self):
+    def update(self) -> None:
         """Move the alien right or left."""
         self.x += self.settings.alien_speed * self.settings.fleet_direction
         self.rect.x = int(self.x)
--- alien_invasion.py
+++ alien_invasion.py
@@ -89,7 +89,7 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
-    def _update_aliens(self):
+    def _update_aliens(self) -> None:
         """Check if the fleet is at an edge, then update positions."""
         self._check_fleet_edges()
         self.aliens.update()
@@ -119,14 +119,14 @@ class AlienInvasion:
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
-    def _check_fleet_edges(self):
+    def _check_fleet_edges(self) -> None:
         """Respond appropriately if any aliens have reached an edge."""
         for alien in self.aliens.sprites():
             if alien.check_edges():
                 self._change_fleet_direction()
                 break
 
-    def _change_fleet_direction(self):
+    def _change_fleet_direction(self) -> None:
         """Drop the entire fleet and change the fleet’s direction."""
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
