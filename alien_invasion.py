03183febc88ab8ba504cf67460b12a93982bddec
nidunc <nidunc@users.noreply.github.com>
2024-10-07 13:29:38 +0200

    Subsection “Repopulating the Fleet”

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,6 +93,11 @@ class AlienInvasion:
         #   If so, get rid of the bullet and the alien.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
+        if not self.aliens:
+            # Destroy existing vbullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+
     def _update_aliens(self) -> None:
         """Check if the fleet is at an edge, then update positions."""
         self._check_fleet_edges()
