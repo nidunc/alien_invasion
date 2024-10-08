44d0e4f2ae6df62f404429f2002bb1fb8f149e39
nidunc <nidunc@users.noreply.github.com>
2024-10-08 11:40:36 +0200

    Subsection “Displaying the Level”

--- scoreboard.py
+++ scoreboard.py
@@ -18,6 +18,7 @@ class Scoreboard:
         # Prepare initial score images.
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         """Turn the score into a rendered image."""
@@ -45,6 +46,18 @@ class Scoreboard:
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
+    def prep_level(self):
+        """Turn the level into a rendered image."""
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(
+            level_str, True, self.text_colour, self.settings.bg_colour
+        )
+
+        # Centre the high score at the top of the screen.
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
     def check_high_score(self):
         """Check to see if there’s a new high score."""
         if self.stats.score > self.stats.high_score:
@@ -52,6 +65,7 @@ class Scoreboard:
             self.prep_high_score()
 
     def show_score(self):
-        """Draw score to the screen."""
+        """Draw scores and level to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
